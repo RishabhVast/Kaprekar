3ea96054c59ab0936b175081a0913362c8bf72ea
Your Name <you@example.com>
2022-02-14 11:31:00 +0530

    first commit

---
+++ recursion.js
@@ -0,0 +1,23 @@
+ //let num=document.querySelector("#number").value;
+ function kaprekars(){
+    let num=document.querySelector("#number").value;
+    
+ recursion = (num) => {
+    do {
+        let arr = [];
+        let text = num.toString();
+        let numpush = arr.push(text);
+        let splitAss = arr[0].split("");
+        let sortVal = splitAss.sort();
+        let assending = sortVal.join("");
+        let splitDes = sortVal.reverse();
+        let desending = splitDes.join("");
+        document.writeln("<p>Assending No." + assending+"</p>");
+        document.writeln("<p>Dessending No." + desending+"</p>");
+        let difference = desending - assending;
+    document.writeln("<p>difference " + difference+"</p><br/>");
+        num = difference;
+    } while (num != 6174);
+}
+recursion(num);
+ }
